39aa12042c2bbc638a130b588ce51d6f3387bf6d
Carlos Andres Nadalig <c.nadalig@itecriocuarto.org.ar>
2024-04-10 22:53:50 -0300

    added on admin.py filters

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,25 @@ from products.models import *
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price')
+    ordering = ('name', 'price')
+
+    search_fields = ('name', 'price', 'category__name')
+
+    list_filter = ('category',)
+
+    list_editable = ('price',)
+
+    empty_value_display = "No data for this field yet"
+
+    #exculde = ('name',)
+
+    list_display = (
+        'name', 
+        'description', 
+        'price',
+        'category',
+        )
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ('name',)
--- products/models.py
+++ products/models.py
@@ -8,9 +8,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
-    category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
